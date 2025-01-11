c437bf5921914c85b1c77b4290f01b2facced591
Mohammad Faisal <mdfaisalnstu@gmail.com>
2025-01-11 13:51:10 +0100

    Leetcode: 1400- Construct K Palindrome Strings

--- Leetcode_1400.py
+++ Leetcode_1400.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class Solution(object):
     def canConstruct(self, s, k):
         """
@@ -5,3 +8,25 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+        if len(s) < k:
+            return False
+
+        counter = Counter(s)
+        unique_counter = 0
+        for key, value in counter.items():
+            remaining = value % 2
+            unique_counter += remaining
+
+        return k >= unique_counter
+
+
+s = "annabelle"
+k = 2
+
+s = "leetcode"
+k = 3
+
+s = "true"
+k = 4
+sol = Solution()
+print(sol.canConstruct(s, k))
